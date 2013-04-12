244e910657d22aa165f1fff2a7a9022d31395df7
Kumar Bibek <coomar.101@gmail.com>
2013-04-12 12:12:18 +0530

    Changes

--- sample/src/com/darvds/ribbonmenu/sample/RibbonsampleActivity.java
+++ sample/src/com/darvds/ribbonmenu/sample/RibbonsampleActivity.java
@@ -1,60 +1,48 @@
+
 package com.darvds.ribbonmenu.sample;
 
 import android.app.Activity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
-import com.darvds.ribbonmenu.R;
+import com.darvds.ribbonmenu.IRibbonMenuCallback;
 import com.darvds.ribbonmenu.RibbonMenuView;
-import com.darvds.ribbonmenu.iRibbonMenuCallback;
 
-public class RibbonsampleActivity extends Activity implements iRibbonMenuCallback {
+public class RibbonsampleActivity extends Activity implements IRibbonMenuCallback {
     /** Called when the activity is first created. */
-	  
-	private RibbonMenuView rbmView;
-		
-		
-	    @Override
-	    public void onCreate(Bundle savedInstanceState) {
-	        super.onCreate(savedInstanceState);
-	        setContentView(R.layout.main);
-	        
-	        
-	        rbmView = (RibbonMenuView) findViewById(R.id.ribbonMenuView1);
-	        rbmView.setMenuClickCallback(this);
-	        rbmView.setMenuItems(R.menu.ribbon_menu);
-	        
-	         getActionBar().setDisplayHomeAsUpEnabled(true);
-	         
-	        
-	    }
-	    
-	    
-	    
 
-		@Override
-		public boolean onOptionsItemSelected(MenuItem item) {
-			int id = item.getItemId();
+    private RibbonMenuView rbmView;
 
-			if (id == android.R.id.home) {
-				
-				rbmView.toggleMenu();
-				
-				return true;
-			
-			} else {
-				return super.onOptionsItemSelected(item);
-			}
-		}
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
 
+        rbmView = (RibbonMenuView)findViewById(R.id.ribbonMenuView1);
+        rbmView.setMenuClickCallback(this);
+        rbmView.setMenuItems(R.menu.ribbon_menu);
 
+        getActionBar().setDisplayHomeAsUpEnabled(true);
 
+    }
 
-		@Override
-		public void RibbonMenuItemClick(int itemId) {
-			
-			
-			
-			
-		}
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+
+            rbmView.toggleMenu();
+
+            return true;
+
+        } else {
+            return super.onOptionsItemSelected(item);
+        }
+    }
+
+    @Override
+    public void RibbonMenuItemClick(int itemId) {
+
+    }
 }
